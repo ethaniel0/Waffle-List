9bdd65b39f4033a47cff31e3d7cec5db070bfda4
Ethan Horowitz <ethan.horowitz@duke.edu>
2021-11-21 23:54:46 -0500

    edit a todo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useState } from 'react';
 function App() {
   const [add, showAdd] = useState(false);
   const [selectItem, setSelectItem] = useState(-1);
+  const [editItem, setEditItem] = useState(-1);
   const [items, editItems] = useState([
     {
       title: "Something to do",
@@ -24,6 +25,9 @@ function App() {
   const addItem = (item) => {
     editItems([...items, item]);
   }
+  const changeItem = (index, item) => {
+    editItems(items.map((i, ind) => ind === index ? item : i));
+  }
   const deleteItem = (index) => {
     editItems(items.filter(i => i !== items[index]));
   }
@@ -37,7 +41,7 @@ function App() {
           {add && <NewForm addItem={addItem} close={() => showAdd(false)} /> }
         </div>
         {items.map((item, ind) => (
-          <Item details={item} ind={ind} select={setSelectItem} clicked={ind === selectItem} onDelete={deleteItem}  />
+          <Item details={item} ind={ind} select={setSelectItem} edit={setEditItem} clicked={ind === selectItem} editable={ind === editItem} onDelete={deleteItem} editItem={changeItem}  />
         ))}
 
       </div>
